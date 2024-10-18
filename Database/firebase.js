0ca4db7279eba79edeb65f323a29d2df6f371aa6
OmarM <omar.elias.m1995@gmail.com>
2024-10-18 15:00:38 -0300

    -18/10

--- Database/firebase.js
+++ Database/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA8Gqk9RLoDZmTSeXlVCg98AioaT1L1rMg",
   authDomain: "desarrollomovil-proyecto.firebaseapp.com",
@@ -13,5 +10,7 @@ const firebaseConfig = {
   appId: "1:119052581561:web:d6173fa929855731c63968"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
--- screens/Crear.js
+++ screens/Crear.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import { View, Button, TextInput, ScrollView, StyleSheet } from "react-native";
+import { db } from '../Database/firebase'; // Asegúrate de que la ruta sea correcta
+import { addDoc, collection } from "firebase/firestore"; 
 
 const CrearUsuario = () => {
     const [state, setState]= useState({
@@ -11,31 +13,70 @@ const CrearUsuario = () => {
     const handleChangeText = (Nombre, value) => {
         setState({...state, [Nombre]:value})
     }
+    const GuardarUsuario = async () => {
+        const mensajes = []; // Array para almacenar los mensajes de error
+    
+        if (state.Nombre === '') {
+            mensajes.push('Por favor, Ingresar un Nombre');
+        }
+        if (state.Apellido === '') {
+            mensajes.push('Por favor, Ingresar un Apellido');
+        }
+        if (state.Email === '') {
+            mensajes.push('Por favor, Ingresar un Email');
+        }
+        if (state.Telefono === '') {
+            mensajes.push('Por favor, Ingresar un Telefono');
+        }
+    
+        if (mensajes.length > 0) {
+            alert(mensajes.join('\n')); // Muestra todos los mensajes en una sola alerta
+        }  else {
+            try {
+                await addDoc(collection(db, "usuarios"), {
+                    Nombre: state.Nombre,
+                    Apellido: state.Apellido,
+                    Email: state.Email,
+                    Telefono: state.Telefono,
+                });
+                alert('Usuario guardado exitosamente!');
+                setState({ Nombre: '', Apellido: '', Email: '', Telefono: '' });
+            } catch (error) {
+                console.error("Error al guardar el usuario: ", error);
+                alert('Error al guardar el usuario, intenta de nuevo.');
+            }
+        }
+    };
+    
     return (
         <ScrollView style={styles.container}>
             <View style={styles.inputGroup}>
                 <TextInput 
                 placeholder="Nombre"
+                value={state.Nombre}
                 onChangeText={(value) => handleChangeText("Nombre", value)}
                 />
             </View>
             <View style={styles.inputGroup}>
                 <TextInput placeholder="Apellido"
+                value={state.Apellido}
                 onChangeText={(value) => handleChangeText("Apellido", value)}
                 />
             </View>
             <View style={styles.inputGroup}>
                 <TextInput placeholder="Email"
+                value={state.Email}
                 onChangeText={(value) => handleChangeText("Email", value)}
                 />
             </View>
             <View style={styles.inputGroup}>
                 <TextInput placeholder="Telefono"
+                value={state.Telefono}
                 onChangeText={(value) => handleChangeText("Telefono", value)}
                 />
             </View>
             <View style={styles.button}>
-                <Button title="AgregarUsuario" onPress={()=>console.log(state)} />
+                <Button title="AgregarUsuario" onPress={()=>GuardarUsuario()} />
             </View>
         </ScrollView>
     )
